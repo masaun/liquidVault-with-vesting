e2019a896935b91aa521c445e13d6385c87c4ec1
masaun <masanoriuno@gmail.com>
2021-05-14 11:44:17 +0900

    [SC]: Define LpVesting.sol

---
+++ contracts/LpVesting.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.1;
+pragma experimental ABIEncoderV2;
+
+/**
+ * @notice - This contract has a role that  yield farming and vesting for LPs
+ */
+contract LpVesting {
+
+    constructor() public {}
+
+}
